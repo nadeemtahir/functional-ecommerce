1b074cf3cfff1d002698c089b8deaf65771413a2
ManahilNadeem <163245288+nadeemtahir@users.noreply.github.com>
2025-02-09 19:19:45 +0500

    Update index.ts

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { product } from './product'
 import { Category } from './category'
+import orderSchema from './data'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product, Category],
+  types: [product, Category, orderSchema,]
 }
